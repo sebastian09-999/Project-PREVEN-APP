d0776617713c80bc1b5f4f15d62a7ce21a4aeea9
jesus david sanchez <jdsanchez3d@gmail.com>
2026-08-24 10:43:57 -0500

    tablas de chuchin

---
+++ app_preven_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('insertar/', views.insertar_datos_prueba, name='insertar_datos'),
+    path('panel/', views.ver_panel_completo, name='panel'),
+]
--- pr_prevenapp/settings.py
+++ pr_prevenapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_prevenapp'
+    'app_prevenapp',
+    'app_preven_app',
 ]
 
 MIDDLEWARE = [
