6948f2294137e9613bdce822914f75a4ab01b97a
Ary <aryflor4@gmail.com>
2026-08-28 12:10:24 -0500

    Se agrega url de la carpeta app_preven_juliana

--- pr_preven_app/urls.py
+++ pr_preven_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('preven-app/', include('app_preven.urls')),
     path('', views.fn_inicio),
     path('preven-app/', include('app_prevenapp.urls')),
-    path('preven-app/', include('app_preven_app.urls'))
+    path('preven-app/', include('app_preven_app.urls')),
+    path('preven-app/', include('app_preven_juliana.urls'))
 ]
 
