bf37037408a52dbc4e69d6d2fec8659bd6ad05b0
Ary <aryflor4@gmail.com>
2026-08-28 12:13:48 -0500

    Cambio de nombre app

--- app_preven_app/apps.py
+++ app_preven_app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AppPrevenAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_preven_app' # Debe ser igual al nombre de la carpeta
+    name = 'app_preven_app'
